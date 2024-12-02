c81427c566818d75768c22e9b80746b700c85337
ANIKMONDALB <158838149+ANIKMONDALB@users.noreply.github.com>
2024-12-02 18:16:20 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const BOT_NUMBER = "918617340627"
+const SESSION_ID = "PRABATH-MD~qbBBiBKb#nQ0auzos0dZzmFmf2GlWULRgnodOTaeZnYGFFckyJOc"
 
 
 module.exports = {
